d10ad9beb42421bb371b1b109531dd010557b802
Svathna <s.s.vathna@gmail.com>
2020-07-01 21:53:26 +0700

    heeh

--- src/app/interfaces/tax-per-month.interface.ts
+++ src/app/interfaces/tax-per-month.interface.ts
@@ -1,7 +1,7 @@
-export interface TaxPerMonth {
+export class TaxPerMonth {
+    month: number;
     revenue: number;
     spending: number;
     taxPaidAmout: number;
     others: string;
-    month: string;
 }
