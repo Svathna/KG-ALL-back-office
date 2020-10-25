49565d0128a695dc8ca5fb15a6405c63c314f4c4
Svathna <s.s.vathna@gmail.com>
2020-10-25 15:11:53 +0700

    fix env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 
 export const environment = {
   production: true,
-  apiURL: "http://localhost:5000",
+  // apiURL: "http://localhost:3000",
+  apiURL: "https://kg-all.herokuapp.com",
 
   CLOUD_NAME: "sva",
   CLOUD_API_KEY: 975547421473123,
